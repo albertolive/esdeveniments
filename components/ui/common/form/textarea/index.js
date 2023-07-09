8876b50574d12e32128eb46233f8d1abdba20851
Albert Olivé <albertolivecorbella@gmail.com>
2023-07-09 09:25:44 +0200

    Fixed issue

--- components/ui/common/form/textarea/index.js
+++ components/ui/common/form/textarea/index.js
@@ -1,5 +1,5 @@
 import dynamic from "next/dynamic";
-const ReactQuill = dynamic(() => import("react-quill"), { noSSR: false });
+const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 import "react-quill/dist/quill.snow.css";
 
 const modules = {
