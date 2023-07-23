874e7108daaf6301ac020513dfd610134eedcb8d
Albert Olivé <albertolivecorbella@gmail.com>
2023-07-23 20:09:46 +0200

    fixing zoom select

--- components/partials/seo-meta.js
+++ components/partials/seo-meta.js
@@ -15,7 +15,7 @@ const Meta = (props) => {
     <Head>
       <title>{props.title}</title>
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
       <meta
         name="robots"
         content={
