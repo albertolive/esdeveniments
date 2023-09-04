c186754ed4579596b30fcaf5929d2d0bdc739cf1
Albert Olivé <albertolivecorbella@gmail.com>
2023-09-04 16:33:07 +0200

    testing issues

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 const siteUrl =
-  process.env.NODE_ENV !== "production" ||
+  process.env.NODE_ENV !== "production" &&
   process.env.VERCEL_ENV !== "production"
     ? "http://localhost:3000"
     : `https://${process.env.VERCEL_URL}`;
