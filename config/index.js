0a62cb4290a773cceb73be38228ddf2dc5da06d9
Albert Olivé <albertolivecorbella@gmail.com>
2023-09-05 08:48:39 +0200

    testing

--- config/index.js
+++ config/index.js
@@ -1,9 +1,9 @@
 const siteUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : process.env.VERCEL_ENV === "preview" ||
-      process.env.VERCEL_ENV === "development"
+    : process.env.NEXT_PUBLIC_VERCEL_ENV === "preview" ||
+      process.env.NEXT_PUBLIC_VERCEL_ENV === "development"
     ? "https://esdeveniments.vercel.app"
     : "https://www.esdeveniments.cat";
-console.log("VERCEL_ENV", process.env.VERCEL_ENV);
+
 export { siteUrl };
