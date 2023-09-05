64f8ff136aa42d173d081f4e754a52ad1dee3b11
Albert Olivé <albertolivecorbella@gmail.com>
2023-09-05 08:25:02 +0200

    testing

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 let siteUrl = process.env.VERCEL_URL;
-if (siteUrl) siteUrl = `https://${vc}`;
+if (siteUrl) siteUrl = `https://${siteUrl}`;
 else siteUrl = "http://localhost:8081";
 
 export { siteUrl };
