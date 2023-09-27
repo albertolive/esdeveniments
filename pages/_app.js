dd21146406a62dbc7c725b68dc059f092a966615
Albert Olive <albertolivecorbella@gmail.com>
2023-09-27 15:24:22 +0200

    Removed unused images and placed in other place + added google ads script

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import "@styles/globals.css";
-import "@styles/fonts.css"
+import "@styles/fonts.css";
 
 import { useEffect, useCallback, memo, Suspense } from "react";
 import Script from "next/script";
@@ -83,6 +83,32 @@ function EsdevenimentsMainEntry({ Component, pageProps }) {
         crossOrigin="anonymous"
         src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADS}`}
       />
+
+      <Script
+        src="https://fundingchoicesmessages.google.com/i/pub-2456713018173238?ers=1"
+        strategy="lazyOnload"
+      />
+
+      <Script id="google-adblock" strategy="lazyOnload">
+        {`
+          (function() {
+            function signalGooglefcPresent() {
+              if (!window.frames['googlefcPresent']) {
+                if (document.body) {
+                  const iframe = document.createElement('iframe');
+                  iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;';
+                  iframe.style.display = 'none';
+                  iframe.name = 'googlefcPresent';
+                  document.body.appendChild(iframe);
+                } else {
+                  setTimeout(signalGooglefcPresent, 0);
+                }
+              }
+            }
+            signalGooglefcPresent();
+          })();
+        `}
+      </Script>
 
       <BaseLayout>
         <Suspense fallback={<></>}>
