5c33fea9fce2c5e82e12af9ac6cce54ab70d0e76
Albert Olive <albertolivecorbella@gmail.com>
2024-01-24 14:41:07 +0100

    Fixed analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import { Analytics } from "@vercel/analytics/react";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -40,7 +39,6 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <Analytics />
         </body>
       </Html>
     );
