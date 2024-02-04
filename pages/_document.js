3db786c9d58cc0f4ec547a6d9f9ffce605e1f872
Albert Olive <albertolivecorbella@gmail.com>
2024-02-04 12:24:42 +0100

    Added meticulous for testing

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,15 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          {(process.env.NODE_ENV === "development" ||
+            process.env.VERCEL_ENV === "preview") && (
+            // eslint-disable-next-line @next/next/no-sync-scripts
+            <script
+              data-project-id={process.env.NEXT_PUBLIC_METICULOUS_PROJECT_ID}
+              data-is-production-environment="false"
+              src="https://snippet.meticulous.ai/v1/meticulous.js"
+            />
+          )}
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
