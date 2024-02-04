7cb1e251ec65f87b09a216bc5746a10c21d6497a
Albert Olive <albertolivecorbella@gmail.com>
2024-02-04 11:56:07 +0100

    testing

--- pages/sitemap/[town]/[year]/[month]/index.js
+++ pages/sitemap/[town]/[year]/[month]/index.js
@@ -55,47 +55,9 @@ export default function Month({ events, town }) {
 }
 
 export async function getStaticPaths() {
-  const { getAllYears } = require("@lib/dates");
-  const {
-    generateRegionsOptions,
-    generateTownsOptions,
-  } = require("@utils/helpers");
-
-  const regions = generateRegionsOptions();
-  const years = getAllYears();
-  let params = [];
-
-  // Get the current year and the next three months
-  const currentYear = new Date().getFullYear();
-  const currentMonth = new Date().getMonth();
-  const nextThreeMonths = currentMonth + 2;
-
-  years.map((year) => {
-    MONTHS_URL.map((month, index) => {
-      // Only pre-render pages for the current year and the next three months
-      if (
-        year < currentYear ||
-        (year === currentYear && index <= nextThreeMonths)
-      ) {
-        regions.map((region) => {
-          const towns = generateTownsOptions(region.value);
-          towns.map((town) => {
-            params.push({
-              params: {
-                town: town.value,
-                year: year.toString(),
-                month: month.toLowerCase(),
-              },
-            });
-          });
-        });
-      }
-    });
-  });
-
   return {
-    paths: params,
-    fallback: true, // Generate remaining pages on-demand
+    paths: [],
+    fallback: "blocking",
   };
 }
 
