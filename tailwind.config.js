16062a3923004b87d69bdcf40090a000172bc5ba
Albert Olive <albertolivecorbella@gmail.com>
2024-02-25 19:54:04 +0100

    Fix things

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,8 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-
 module.exports = {
   purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
   content: [
     "./components/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
-  ],
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
