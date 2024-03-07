ab3c0bc1b1ec99da059328a4b22f4a35adc3709f
Albert Olive <albertolivecorbella@gmail.com>
2024-03-07 14:28:16 +0100

    Improved code

--- components/ui/eventsAroundScroll/index.js
+++ components/ui/eventsAroundScroll/index.js
@@ -3,6 +3,18 @@ import Link from "next/link";
 import { truncateString } from "@utils/helpers";
 import Image from "@components/ui/common/image";
 
+const sendEventClickGA = (eventId, eventTitle) => {
+  if (typeof window !== "undefined" && window.gtag) {
+    window.gtag("event", "select_content", {
+      content_type: "event",
+      item_id: eventId,
+      item_name: eventTitle,
+      event_category: "Events Around",
+      event_label: eventTitle,
+    });
+  }
+};
+
 function EventCardLoading() {
   return (
     <div className="flex-none w-40 min-w-[10rem] flex flex-col bg-whiteCorp overflow-hidden cursor-pointer animate-pulse">
@@ -37,38 +49,37 @@ function EventsAroundScroll({ events, loading }) {
         const title = truncateString(event.title || "", 60);
 
         return (
-          <Link
+          <div
             key={event.id}
-            href={`/e/${event.slug}`}
-            passHref
-            prefetch={false}
+            className="flex-none w-40 min-w-[10rem] flex flex-col bg-white overflow-hidden cursor-pointer"
           >
-            <div className="flex-none w-40 min-w-[10rem] flex flex-col bg-white overflow-hidden cursor-pointer">
-              {/* ImageEvent */}
-              <div className="w-full h-32 flex justify-center items-center overflow-hidden">
-                <Image
-                  className="w-full object-cover"
-                  title={event.title}
-                  image={event.imageUploaded}
-                  alt={event.title}
-                  layout="fill"
-                  priority={true}
-                />
-              </div>
-              {/* Title */}
-              <div className="p-1">
-                <h3 className="text-sm font-semibold text-ellipsis overflow-hidden whitespace-nowrap">
-                  {title}
-                </h3>
-              </div>
-              {/* Location */}
-              <div className="p-1">
-                <div className="text-xs font-normal text-ellipsis overflow-hidden whitespace-nowrap">
-                  <span>{event.location}</span>
+            <Link href={`/e/${event.slug}`} passHref prefetch={false}>
+              <div onClick={() => sendEventClickGA(event.id, event.title)}>
+                {/* ImageEvent */}
+                <div className="w-full h-32 flex justify-center items-center overflow-hidden">
+                  <Image
+                    className="w-full object-cover"
+                    title={event.title}
+                    image={event.imageUploaded}
+                    alt={event.title}
+                    layout="fill"
+                  />
+                </div>
+                {/* Title */}
+                <div className="p-1">
+                  <h3 className="text-sm font-semibold text-ellipsis overflow-hidden whitespace-nowrap">
+                    {title}
+                  </h3>
+                </div>
+                {/* Location */}
+                <div className="p-1">
+                  <div className="text-xs font-normal text-ellipsis overflow-hidden whitespace-nowrap">
+                    <span>{event.location}</span>
+                  </div>
                 </div>
               </div>
-            </div>
-          </Link>
+            </Link>
+          </div>
         );
       })}
     </div>
