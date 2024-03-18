fb6a8e5ef9e34914c32a50b7610f18193b1870ef
Albert Olive <albertolivecorbella@gmail.com>
2024-03-18 10:00:02 +0100

    Removed setTimeOut

--- pages/api/insertTowns.js
+++ pages/api/insertTowns.js
@@ -27,7 +27,6 @@ export default async function handler(req, res) {
 
     for (const url of urls) {
       await fetchDataForTown(url);
-      await new Promise((resolve) => setTimeout(resolve, 1000));
     }
 
     const removeDuplicatesUrl = `${siteUrl}/api/removeDuplicates`;
