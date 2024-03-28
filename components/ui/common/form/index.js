933d8469bde5c0794007d787c62483dddc2681d0
Albert Olive <albertolivecorbella@gmail.com>
2024-03-28 14:26:10 +0100

    Improved performance

--- components/ui/common/form/index.js
+++ components/ui/common/form/index.js
@@ -2,5 +2,4 @@ export { default as TextArea } from "./textarea";
 export { default as Input } from "./input";
 export { default as DatePicker } from "./datePicker";
 export { default as Select } from "./select";
-export { default as FrequencySelect } from "./frequencySelect";
 export { default as ImageUpload } from "./imageUpload";
