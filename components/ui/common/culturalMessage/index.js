9cf74fe89ea2992d237e34d25d35beab322e9b94
Albert Olive <albertolivecorbella@gmail.com>
2024-03-29 07:16:06 +0100

    Added GA

--- components/ui/common/culturalMessage/index.js
+++ components/ui/common/culturalMessage/index.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { getTownValueByLabel } from "@utils/helpers";
+import { sendGoogleEvent } from "@utils/analytics";
 
 const CulturalMessage = ({ location }) => {
   const town = getTownValueByLabel(location);
@@ -8,29 +9,46 @@ const CulturalMessage = ({ location }) => {
     return null;
   }
 
+  const handleNavigation = (town, timeframe) => {
+    const eventLabel = `navigate_to_${town}_${timeframe}`;
+    sendGoogleEvent(eventLabel);
+  };
+
   return (
     <p className="mt-2">
       Imagina un lloc on cada dia és una nova descoberta. Això és{" "}
       <span className="font-bold">{location}</span>: un univers de cultura
       esperant ser explorat per tu. Comença la teva aventura{" "}
-      <Link href={`/${town}/avui`}>
-        <span className="font-medium text-primary hover:underline">avui</span>
+      <Link
+        href={`/${town}/avui`}
+        onClick={() => handleNavigation(town, "avui")}
+        className="font-medium text-primary hover:underline"
+      >
+        avui
       </Link>
       , descobreix què està passant{" "}
-      <Link href={`/${town}/dema`}>
-        <span className="font-medium text-primary hover:underline">demà</span>
+      <Link
+        href={`/${town}/dema`}
+        onClick={() => handleNavigation(town, "dema")}
+        className="font-medium text-primary hover:underline"
+      >
+        demà
       </Link>
       , continua explorant{" "}
-      <Link href={`/${town}/setmana`}>
-        <span className="font-medium text-primary hover:underline">
-          durant la setmana
-        </span>
+      <Link
+        href={`/${town}/setmana`}
+        onClick={() => handleNavigation(town, "setmana")}
+        className="font-medium text-primary hover:underline"
+      >
+        durant la setmana
       </Link>
       , i culmina amb un{" "}
-      <Link href={`/${town}/cap-de-setmana`}>
-        <span className="font-medium text-primary hover:underline">
-          cap de setmana espectacular
-        </span>
+      <Link
+        href={`/${town}/cap-de-setmana`}
+        onClick={() => handleNavigation(town, "cap-de-setmana")}
+        className="font-medium text-primary hover:underline"
+      >
+        cap de setmana espectacular
       </Link>
       . Deixa&apos;t sorprendre per tot el que{" "}
       <span className="font-bold">{location}</span> pot oferir-te.
