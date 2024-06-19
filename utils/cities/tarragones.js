fccff90603e61cb3698259a95061e7d4b068a7ea
Albert Olive <albertolivecorbella@gmail.com>
2024-06-19 12:11:11 +0200

    fixed tarragones

--- utils/cities/tarragones.js
+++ utils/cities/tarragones.js
@@ -1,57 +1,53 @@
-import {siteUrl} from "@config/index";
-
-const sharedData = {
-    descriptionSelector: ".ddbbtext",
-    imageSelector: ".first-image",
-};
+import { siteUrl } from "@config/index";
 
 const tarragones = [
-    "tarragones",
-    {
-        label: "Tarragonés",
-        towns: new Map([
-            [
-                "bot",
-                {
-                    label: "Bot",
-                    rssFeed: `${siteUrl}/api/scrapeEvents?city=bot`,
-                    postalCode: "43785",
-                    coords: { lat: 41.009169, lng: 0.384250 },
-                    getDescriptionFromRss: true,
-                },
-            ],
-            [
-                "la-nou-de-gaia",
-                {
-                    label: "La Nou de Gaià",
-                    rssFeed: `${siteUrl}/api/scrapeEvents?city=la-nou-de-gaia`,
-                    postalCode: "43763",
-                    coords: { lat: 41.182941, lng: 1.373928 },
-                    getDescriptionFromRss: true,
-                },
-            ],
-            [
-                "altafulla",
-                {
-                    label: "Altafulla",
-                    rssFeed: `${siteUrl}/api/scrapeEvents?city=altafulla`,
-                    postalCode: "43893",
-                    coords: { lat: 41.142162, lng: 1.376840 },
-                    getDescriptionFromRss: true,
-                }
-            ],
-            [
-                "tarragona",
-                {
-                    label: "Tarragona",
-                    rssFeed: `${siteUrl}/api/scrapeEvents?city=tarragona`,
-                    postalCode: "43005",
-                    coords: { lat: 41.119221, lng: 1.245897 },
-                    getDescriptionFromRss: true,
-                }
-            ]
-        ]),
-    },
+  "tarragones",
+  {
+    label: "Tarragonés",
+    province: "tarragona",
+    towns: new Map([
+      [
+        "bot",
+        {
+          label: "Bot",
+          rssFeed: `${siteUrl}/api/scrapeEvents?city=bot`,
+          postalCode: "43785",
+          coords: { lat: 41.009169, lng: 0.38425 },
+          getDescriptionFromRss: true,
+        },
+      ],
+      [
+        "la-nou-de-gaia",
+        {
+          label: "La Nou de Gaià",
+          rssFeed: `${siteUrl}/api/scrapeEvents?city=la-nou-de-gaia`,
+          postalCode: "43763",
+          coords: { lat: 41.182941, lng: 1.373928 },
+          getDescriptionFromRss: true,
+        },
+      ],
+      [
+        "altafulla",
+        {
+          label: "Altafulla",
+          rssFeed: `${siteUrl}/api/scrapeEvents?city=altafulla`,
+          postalCode: "43893",
+          coords: { lat: 41.142162, lng: 1.37684 },
+          getDescriptionFromRss: true,
+        },
+      ],
+      [
+        "tarragona",
+        {
+          label: "Tarragona",
+          rssFeed: `${siteUrl}/api/scrapeEvents?city=tarragona`,
+          postalCode: "43005",
+          coords: { lat: 41.119221, lng: 1.245897 },
+          getDescriptionFromRss: true,
+        },
+      ],
+    ]),
+  },
 ];
 
 export default tarragones;
