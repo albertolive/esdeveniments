80ddbc4d35a9c8088018f41c4af476f0b9544eae
Albert Olive <albertolivecorbella@gmail.com>
2024-06-25 10:08:46 +0200

    test

---
+++ pages/api/edge-function-rss-fetch.js
@@ -0,0 +1,37 @@
+// pages/api/edge-fetch-rss.js
+
+export const config = {
+  runtime: 'edge',
+};
+
+export default async function handler(req) {
+  const rssFeed = 'https://www.oris.cat/rss/12/0/';
+
+  try {
+    const response = await fetch(rssFeed, {
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.text();
+    return new Response(JSON.stringify({ success: true, data }), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  } catch (error) {
+    console.error('Error fetching RSS feed:', error);
+    return new Response(JSON.stringify({ success: false, error: error.message }), {
+      status: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+}
