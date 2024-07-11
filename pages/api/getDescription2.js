b9683d496e24b2638e07c0815af4c5890e3e8969
Albert Olive <albertolivecorbella@gmail.com>
2024-07-11 14:09:39 +0200

    test

--- pages/api/getDescription2.js
+++ pages/api/getDescription2.js
@@ -30,7 +30,10 @@ async function fetchWithPuppeteer(url) {
     });
 
     const page = await browser.newPage();
-    await page.goto(url, { waitUntil: "networkidle2" });
+    await page.setUserAgent(
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    );
+    await page.goto(url, { waitUntil: "networkidle2", timeout: 60000 });
 
     const html = await page.content();
     return html;
